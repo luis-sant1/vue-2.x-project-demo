ad7a39f255ce293d464d2e851c4d5e62bb3b876b
luis-sant1 <santiagomuchacholuma@uvm.edu.ve>
2023-12-19 22:01:52 -0600

    Add Quill

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LandingPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import LandingPage from './components/Landing.vue';
 export default {
   name: 'App',
+  data() {
+    return {message: 'Hello World'}
+  } ,
   components: {
-    HelloWorld
+    LandingPage
   }
 }
 </script>
---
+++ src/components/Landing.vue
@@ -0,0 +1,35 @@
+<!-- Imports -->
+<script>
+import { VueEditor } from "vue2-editor";
+export default {
+    name: 'LandingPage',
+    components: {
+        VueEditor
+    },
+    data() {
+        return {
+            content: `
+            <h1> this header </h1>
+            <p>Amet irure excepteur cillum ipsum sit elit dolor et nisi eu consectetur aliqua laborum laboris.</p>
+            <h1> This Footer</h1>
+            `
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+        <h1>
+            Demo AMII
+        </h1>
+
+        <div id="app">
+            <VueEditor v.model = "content"/>
+        </div>
+    </main>
+</template>
+
+
+
+<style scoped></style>
